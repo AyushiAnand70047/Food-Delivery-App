605a6843c7189300815a0da42c2577c8cc22d28d
AyushiAnand70047 <ayushianand088@gmail.com>
2024-04-01 11:16:28 +0530

    MERN Project

---
+++ GoFood/mernapp/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+// import Navbar from './components/Navbar';
+import Home from './screens/Home';
+
+function App() {
+  return (
+    <>
+    <div><Home/></div>
+    </>
+  );
+}
+
+export default App;
