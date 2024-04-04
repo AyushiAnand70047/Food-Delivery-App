74ba64228593a4e5bb2505dd2c3c404c633fc5d2
AyushiAnand70047 <ayushianand088@gmail.com>
2024-04-04 09:50:09 +0530

    Poject

--- GoFood/mernapp/src/screens/Home.js
+++ GoFood/mernapp/src/screens/Home.js
@@ -1,13 +1,27 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Card from "../components/Card";
+import Crousel from "../components/Crousel";
 
 export default function Home() {
   return (
     <div>
-        <div><Navbar/></div>
-        <div>Body</div>
-        <div><Footer/></div>
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <Crousel/>
+      </div>
+      <div className="m-3">
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+      </div>
+      <div>
+        <Footer />
+      </div>
     </div>
-  )
+  );
 }
